2f5b831949581bf4d3224d8b1f31cde2afbc7dc6
FMGX11 <0619970659@uma.es>
2021-03-09 10:13:21 +0100

    A ver si ahora

--- Jarra.java
+++ Jarra.java
@@ -1,38 +1,24 @@
 public class Jarra {//variables de instancia
     private int cantidad;
     private int capacidad;
-    public Jarra(int capacidadInicial){
 
-    if(capacidadInicial<=0){
-    throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero.");
+    public Jarra(int capacidadInicial) {
+
+        if (capacidadInicial <= 0) {
+            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero.");
+        }
+        this.capacidad = capacidadInicial;
     }
-        this.capacidad=capacidadInicial;
+
+    public void llenaDesde(Jarra j) {
+        while ((this.cantidad < this.capacidad) && (j.cantidad > 0)) {
+            this.cantidad++;
+            j.cantidad--;
+        }
     }
-        // Aquí vendrán después los métodos creados en la siguiente fase
-<<<<<<< HEAD
-        public void metodoComunitario() {
-            // Comentario incluido para pruebas
-            System.out.println("Aqui escribimos todos");
 
-        }
-
-        public void llenaDesde(Jarra j){
-            while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
-                this.cantidad++;
-                j.cantidad--;
-            }
-        }
-
-        public String toString(){
-            return ("("+this.capacidad+","+this.cantidad+")");
-        }
+    public String toString() {
+        return ("(" + this.capacidad + "," + this.cantidad + ")");
+    }
 }
-=======
-        public void metodoComunitario(){
-        // Comentario incluido para pruebas
-        System.out.println("Aqui escribimos todos");
-    }
-    
-    //Esto lo estoy haciendo desde el navegador, soy la parte D
-}
->>>>>>> 9a1732545e8b26b676eeabaa2950bdac28b609f5
+
