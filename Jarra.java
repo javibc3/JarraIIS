91639b69d852336e228f37d21512b483c3e28c70
Mario <marioclementramirez@gmail.com>
2021-03-11 10:23:06 +0100

    ParteB

--- Jarra.java
+++ Jarra.java
@@ -1,37 +1,7 @@
-﻿public class Jarra {//variables de instancia
-    private int cantidad;
-    private int capacidad;
+﻿public int capacidad(){
+	return this.capacidad;
+}
 
-    public Jarra(int capacidadInicial) {
-
-        if (capacidadInicial <= 0) {
-            throw new RuntimeException("ERROR: no se pueden crear jarras con capacidad menor o igual que cero.");
-        }
-        this.capacidad = capacidadInicial;
-    }
-
-    public void llenaDesde(Jarra j) {
-        while ((this.cantidad < this.capacidad) && (j.cantidad > 0)) {
-            this.cantidad++;
-            j.cantidad--;
-        }
-    }
-        
-    public String toString(){
-        return ("("+this.capacidad+","+this.cantidad+")");
-    }
-
-    // Aquí vendrán después los métodos creados en la siguiente fase
-	public int capacidad(){
-		return this.capacidad;
-	}
-
-	public int cantidad(){
-		return this.cantidad;
-	}
-
-    public void metodoComunitario(){
-        // Comentario incluido para pruebas
-        System.out.println("Aqui escribimos todos");
-    }
+public int cantidad(){
+	return this.cantidad;
 }
